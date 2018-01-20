aa7c256659d3fbae05bde11b4040e56aabd38651
richardson-marc <marc.richardson@leankit.com>
2018-01-20 17:03:21 -0500

    working code

--- palindrome.pl
+++ palindrome.pl
@@ -1,40 +1,40 @@
 #!/usr/bin/env perl
 
-#use strict;
-#use warnings;
+use strict;
+use warnings;
+
+my $addition;
+my $reverse;
+my $debug;
 
 print 'Enter string: ';
 chomp(my $string = <STDIN>);
 
-#print $string eq reverse($string) ? 'Yes' : 'No', "\n";
 
 sub check_palindrome {
+    my($string) = @_;
 if ($string == reverse($string) ) {
-    print "this is a palindrome\n";
+    print "$string is a palindrome\n";
+    exit;
 } else {
-    print "this is not a palindrome\n";
+    print "$string is not a palindrome\n" if $debug;
     my $reverse = reverse($string);
     add_reverse($string,$reverse);
 }
 }
 sub add_reverse {
-    print "running add_reverse\n";
     my ($string, $reverse) = @_;
-    print "running add_reverse\n";
-       print "reverse is $reverse\n";
+#       print "reverse is $reverse\n";
     my $addition = $string + reverse($string);
-    print "addition is $addition\n";
-#
+    print "$addition\n";
+    check_palindrome($addition);
     print "addition $addition reverse $reverse\n";
         return ($addition, $reverse);
 }
 
-check_palindrome();
+check_palindrome($string);
 
 sub_eval($addition, $reverse);
-#    my $reverse = reverse($addition);
- 
-#    return ($addition, $reverse, $string);
 sub sub_eval {
     my ($addition, $reverse) = @_;
 	print "compare $addition $reverse\n";
@@ -42,7 +42,6 @@ sub sub_eval {
 
 	print "first time failed\n";
 	print "$addition is not a palindrome\n";
-#	add_reverse();
 	}
 }    
        
